14b9054e9702ca2dac5088f8ab95577abad75b96
Marcel Hauf <marcel.hauf@n-fuse.de>
2016-08-15 13:37:35 +0200

    Add kafka protobuf map test

--- test/kafka_test.js
+++ test/kafka_test.js
@@ -6,6 +6,8 @@ const coMocha = require('co-mocha');
 coMocha(mocha);
 
 const should = require('should');
+const _ = require('lodash');
+const sync = require('gostd').sync;
 const logger = require('./logger_test.js');
 const chassis = require('../');
 const config = chassis.config;
@@ -22,6 +24,34 @@ describe('Kafka events provider', () => {
   });
   after(function* stopProvider() {
     yield events.end();
+  });
+  describe('topic.on', function topicOn() {
+    this.timeout(5000);
+    it('should receive the correct message', function* checkSendReceive() {
+      const topic = yield events.topic('test.wait');
+      const testMessage = {
+        value: 'test',
+        count: 1,
+        kv: {},
+      };
+      const wg = new sync.WaitGroup();
+      let err;
+      yield topic.on('test-event', function* onTestEvent(message, context) {
+        try {
+          should.exist(message);
+          should.exist(message.kv);
+          const keys = _.keys(message.kv);
+          keys.should.equal([]);
+        } catch (error) {
+          err = error;
+        }
+        wg.done();
+      });
+      wg.add(1);
+      yield topic.emit('test-event', testMessage);
+      yield wg.wait();
+      should.ifError(err);
+    });
   });
   describe('topic.$wait', function testWait() {
     this.timeout(5000);
